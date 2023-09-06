1e7a6680a126d311656e2127201cd412b492ee0f
SaBiT495 <shahedbillah495@gmail.com>
2023-09-06 17:09:34 +0600

    signup login and category create done and using firebase realtime data

--- firebase.js
+++ firebase.js
@@ -2,19 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getDatabase } from 'firebase/database'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8cnJOj3mcFeWzNvr5jDXTBVyyABPk7hU",
-  authDomain: "authapp-b7a86.firebaseapp.com",
-  projectId: "authapp-b7a86",
-  storageBucket: "authapp-b7a86.appspot.com",
-  messagingSenderId: "549645649211",
-  appId: "1:549645649211:web:c55d1cc369d712dff6631e"
-};
+    apiKey: "AIzaSyA8cnJOj3mcFeWzNvr5jDXTBVyyABPk7hU",
+    authDomain: "authapp-b7a86.firebaseapp.com",
+    databaseURL: "https://authapp-b7a86-default-rtdb.firebaseio.com",
+    projectId: "authapp-b7a86",
+    storageBucket: "authapp-b7a86.appspot.com",
+    messagingSenderId: "549645649211",
+    appId: "1:549645649211:web:c55d1cc369d712dff6631e"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -22,5 +24,7 @@ const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
 // const auth = getAuth(app)
+const db = getDatabase();
 
+export { db }
 export { auth };
--- layout/HomePage.jsx
+++ layout/HomePage.jsx
@@ -1,12 +1,129 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Alert, Button, FlatList, Modal, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { db } from '../firebase'
+import { ref, set, get } from 'firebase/database'
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid'; 
 
-const HomePage = () => {
-  return (
-    <View>
-      <Text>Congratulation you are log in your account</Text>
-    </View>
-  )
-}
 
-export default HomePage
+  
+export default function HomePage() {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [textValue, setTextValue] = useState('');
+    const [cates, setCates] = useState([]);
+
+    const toSnakeCase = (str = '') => {
+        str = str.trim();
+        const strArr = str.split(' ');
+        const snakeArr = strArr.reduce((acc, val) => {
+            return acc.concat(val.toLowerCase());
+        }, []);
+        return snakeArr.join('_');
+    };
+
+    // console.log(totalBalance);
+
+    const navigation = useNavigation();
+
+    const handleOpenModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleSaveModal = () => {
+        set(ref(db, 'category/' + toSnakeCase(textValue)), {
+            id: uuidv4(),
+            title: textValue,
+        });
+        // Do something with textValue and numberValue
+        console.log('Text:', textValue);
+        setCates([...cates, { name: textValue }]);
+        setTextValue('')
+        setIsModalVisible(false);
+    };
+
+    const handleListItemPress = (categoryName) => {
+        navigation.navigate('CatePage', { categoryName });
+        // Navigate to the new component and pass data
+    };
+
+    useEffect(() => {
+        let itemsRef = ref(db, 'category');
+        get(itemsRef)
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    const allData = snapshot.val();
+                    const itemsList = Object.values(allData);
+                    setCates(itemsList);
+                    // console.log("All retrieved data:", itemsList);
+                } else {
+                    console.log("No data available");
+                }
+            })
+            .catch((error) => {
+                // console.error("Error retrieving data:", error);
+            });
+
+    }, [cates])
+
+    return (
+        <>
+            <View className="bg-orange-300 flex-1">
+                <ScrollView className="bg-gray-300">
+                    <View className="flex m-10">
+                        <View className="flex justify-between flex-row">
+                            <Text className="text-2xl font-bold">Category</Text>
+                            <Text className="text-2xl font-bold">Balance</Text>
+                        </View>
+                        {cates.map((cate, index) => (
+                            <TouchableOpacity
+                                key={index}
+                                className="bg-white rounded-lg p-5 my-2 shadow flex-1"
+                                onPress={() => handleListItemPress(cate.title)}
+                            >
+
+                                <View className="flex justify-between flex-row">
+                                    <Text className="text-2xl">{cate.title}</Text>
+                                    <Text className="text-2xl">200.00</Text>
+                                </View>
+                            </TouchableOpacity>
+                        ))}
+                    </View>
+                </ScrollView>
+                <TouchableOpacity className="absolute opacity-6 bottom-10 right-10 bg-sky-700 rounded-full w-16 h-16 flex justify-center items-center line-clamp-1 shadow" onPress={handleOpenModal}>
+                    <Text className="text-4xl text-white">+</Text>
+                </TouchableOpacity>
+            </View>
+
+
+
+            <Modal visible={isModalVisible} animationType="fade" transparent>
+                <View className="flex-1 items-center justify-center" style={{ 'backgroundColor': 'rgba(0,0,0,.8)' }}>
+
+
+                    <View className="bg-orange-50 w-80 px-5 pb-10 pt-5 rounded-md">
+                        <Text className="mb-5 text-lg">Insert your category name</Text>
+                        <TextInput
+                            className="border border-gray-400 rounded-md w-full mb-6 p-3 text-lg"
+                            placeholder="Enter text"
+                            onChangeText={setTextValue}
+                            value={textValue}
+                        />
+                        <View className="flex flex-row gap-5">
+                            <TouchableOpacity className="flex-1 bg-red-500 rounded-md p-2.5" onPress={handleCloseModal}>
+                                <Text className="text-center text-lg text-white"  >Close</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity className="flex-1 bg-cyan-500 p-2.5 rounded-md" onPress={handleSaveModal}>
+                                <Text className="text-center text-lg text-white" >Save</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+        </>
+    );
+}         
