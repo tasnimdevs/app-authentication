87d6d93447bf1d2c00469b02fe1c2b8a32080aac
SaBiT495 <shahedbillah495@gmail.com>
2023-09-06 16:48:05 +0600

    Navigation fix

--- layout/HomePage.jsx
+++ layout/HomePage.jsx
@@ -1,12 +1,10 @@
 import { Text, View } from 'react-native'
 import React from 'react'
-import AppDrawer from './AppDrawer'
 
 const HomePage = () => {
   return (
     <View>
       <Text>Congratulation you are log in your account</Text>
-      <AppDrawer />
     </View>
   )
 }
